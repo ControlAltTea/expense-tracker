4a8d726511012cca12c68897cce2c01ee904c61f
vinson-han <han_vinson@student.smc.edu>
2024-04-22 13:16:39 -0700

    intial loginRoute/loginController & middleware for logging api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,28 @@
+const morgan = require("morgan");
 const express = require("express");
-
 const app = express();
+const cors = require("cors");
+const loginRouter = require("./routes/loginRouter");
+const middleware = require("./middlewares/middleware");
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("tiny"));
+  console.log("testing");
+}
+
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(middleware.requestLogger);
+
+app.use("/api/login", loginRouter);
+
+app.get("/test", (req, res) => {
+  console.log("test");
+});
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
