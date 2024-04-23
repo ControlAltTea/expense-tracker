cfce5ec4c22028028066735862f5c0c0b532bb11
kevinngonzales <kevinngstem@gmail.com>
2024-04-22 18:50:52 -0700

    added nodemon as dev dependency

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -4,17 +4,36 @@ import React from 'react'
 function Login() {
   return (
     <>
+<div className='flex flex-col'>
 
+
+
+<div className='flex justify-center px-6 py-12'>
+  <h2>Sign in to your account</h2>
+</div>
+
+<div className='flex justify-center'>
     <form>
+      <div className='flex justify-center flex-col'>
         <label>Email</label>
         <input className='border-solid border-2 border-black' />
         <label>Password</label>
         <input className='border-solid border-2 border-black'/>
-        <button type='submit'>Login</button>
-    </form>
 
-    <div>Don't Have an Account?</div>
-    <Link to='/signup'>Sign Up</Link>
+          <div className='flex justify-center px-4 py-6'>
+          <button type='submit'>Login</button>
+          </div>
+
+        </div>
+    </form>
+</div>
+
+    <div className='flex items-center flex-col py-4'>
+      <div>Don't Have an Account?</div>
+      <Link to='/signup'>Sign Up</Link>
+    </div>
+
+</div>
     </>
   )
 }
