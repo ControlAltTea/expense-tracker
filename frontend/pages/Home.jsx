fa19e61818504b15ba1998d76cc0feba85f7a2c0
vinson-han <han_vinson@student.smc.edu>
2024-06-16 21:03:48 -0700

    merge conflict

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ExpenseGoals from "../components/ExpensesPage";
 import ChatBar from "../components/ChatBar";
 import IncomeForm from "../components/IncomeComponent/IncomeForm";
@@ -12,14 +11,6 @@ function Home() {
       </div>
     </div>
   );
-=======
-import React from 'react'
-
-function Home() {
-  return (
-    <div>Home</div>
-  )
->>>>>>> 23fea0f02d470ca547d2d4f281bff84d11aa01fe
 }
 
-export default Home
+export default Home;
