ac8a8232ec553c3a8e9a82c8f03c7c565fb753db
vinson-han <han_vinson@student.smc.edu>
2024-06-25 18:44:05 -0700

    css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-    <App />
+      <App />
     </HashRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
